fb7d9d776975f97dcf07c0ee0c97bcf55ede15b0
Amber Lahav <amberlahav@gmail.com>
2017-11-22 17:30:40 -0500

    Added welcome page

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,24 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const donuts= ['Sourcream Glazed', 'Honey Dip', 'Boston Cream'];
-import Donut from './donut'
 
+var config = {
+  apiKey: "AIzaSyAnY0sZu5MdoXZq44UIAMUPXzEFdVlohpw",
+  authDomain: "virtual-plant.firebaseapp.com",
+  databaseURL: "https://virtual-plant.firebaseio.com",
+  projectId: "virtual-plant",
+  storageBucket: "",
+  messagingSenderId: "1073258773152"
+};
+firebase.initializeApp(config);
 
 class App extends React.Component {
+
     render() {
       return (
         <div>
-          {donuts.map(donut => {
-            // map produces a new array with what you are doing with the old array, in this case it's making them a list of h2's
-            return (
-              // donut represents the value of each iteration
-              <Donut donutName={donut}/>
-            )
-          })}
-          <footer>donuts &copy; 2017</footer>
+          <Welcome />
         </div>
       )
     }
+}
+
+class Welcome extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: "",
+      plantName: "",
+      points: 50
+    }
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+   
+  }
+  componentDidMount() {
+    const usersRef = firebase.database().ref();
+
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+ 
+  addUser(e) {
+    e.preventDefault();
+    const usersItem = {
+      userName: this.state.userName,
+      plantName: this.state.plantName,
+      dateClicked: Date.now(),
+      points: 50
+    }
+   console.log(usersItem);
+    const dbRef = firebase.database().ref();
+    dbRef.push(usersItem);
+    this.setState({
+      userName: "",
+      plantName: ""
+    });
+  }
+
+  render() {
+
+    return (
+
+      <form onSubmit={this.addUser}>
+        <h1>Welcome to Virtual Plant!</h1>
+
+        <label htmlFor="userName">Enter your name:</label>
+        <input type="text" onChange={this.handleChange} value={this.state.userName} name="userName" />
+
+        <label htmlFor="plantName">Choose your plant's name:</label>
+        <input type="text" onChange={this.handleChange} value={this.state.plantName} name="plantName" />
+
+        <button type="submit">START</button>
+      </form>
+
+        )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
