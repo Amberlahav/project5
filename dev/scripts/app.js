1911ff5c8caafcba57d5e7194491c8a3dae3c215
Amber Lahav <amberlahav@gmail.com>
2017-11-21 19:16:27 -0500

    Adding donuts

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const donuts= ['Sourcream Glazed', 'Honey Dip', 'Boston Cream'];
+import Donut from './donut'
+
 
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          {donuts.map(donut => {
+            // map produces a new array with what you are doing with the old array, in this case it's making them a list of h2's
+            return (
+              // donut represents the value of each iteration
+              <Donut donutName={donut}/>
+            )
+          })}
+          <footer>donuts &copy; 2017</footer>
         </div>
       )
     }
